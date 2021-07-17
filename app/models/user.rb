6e1cb541daf03caa8a56169d2f149dfb74c0280e
doshara1280 <danyuan749xa@gmail.com>
2021-07-17 18:32:34 +0900

    パスワードのバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :apps
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   with_options presence: true do
     validates :nickname
     validates :one_word_comment
