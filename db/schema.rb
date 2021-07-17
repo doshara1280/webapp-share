12167005703e2e2f3f85d8f0a19bb336ef353ead
doshara1280 <danyuan749xa@gmail.com>
2021-07-17 14:20:42 +0900

    アプリテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_16_114226) do
+ActiveRecord::Schema.define(version: 2021_07_16_231749) do
+
+  create_table "apps", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.text "description", null: false
+    t.string "language", null: false
+    t.text "free", null: false
+    t.string "link", null: false
+    t.integer "edit_count", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_apps_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -26,4 +39,5 @@ ActiveRecord::Schema.define(version: 2021_07_16_114226) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "apps", "users"
 end
