dc4fc013dd97965ffd545d73327dbad0317c72df
doshara1280 <danyuan749xa@gmail.com>
2021-07-17 18:30:20 +0900

    ユーザーテストコード記述

---
+++ spec/factories/users.rb
@@ -0,0 +1,9 @@
+FactoryBot.define do
+  factory :user do
+    nickname              {'test'}
+    one_word_comment      {'test_comment'}
+    email                 {'test@example'}
+    password              {'1a1a1a'}
+    password_confirmation {password}
+  end
+end
