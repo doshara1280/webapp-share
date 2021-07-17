168f86ae0c1fa2e74fc2844a0fff38c4cb0312f3
doshara1280 <danyuan749xa@gmail.com>
2021-07-17 14:27:12 +0900

    ユーザーモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :apps
+  with_options presence: true do
+    validates :nickname
+    validates :one_word_comment
+  end
 end
