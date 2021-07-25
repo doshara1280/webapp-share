cbfc0b8e816567f48a2fdde4c7af66757fb372f5
doshara1280 <danyuan749xa@gmail.com>
2021-07-26 07:56:27 +0900

    パスを直接入力し、投稿したウェブアプリ以外の編集ページへの遷移を防止

--- app/controllers/web_apps_controller.rb
+++ app/controllers/web_apps_controller.rb
@@ -1,5 +1,7 @@
 class WebAppsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :show]
+  before_action :authenticate_user!, only: [:new, :create, :show, :edit, :update]
+  before_action :find, only: [:show, :edit, :update]
+  before_action :move_to_top, only: [:edit, :update]
 
   def index
     @web_apps = WebApp.includes(:user).order('created_at DESC').page(params[:page]).per(3)
@@ -19,11 +21,31 @@ class WebAppsController < ApplicationController
   end
 
   def show 
-    @web_app = WebApp.find(params[:id])
   end
   
+  def edit
+  end
+
+  def update
+    if @web_app.update(web_app_params)
+      redirect_to web_app_path(@web_app.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def web_app_params
     params.require(:web_app).permit(:name, :description, :language, :free, :link, :edit_count, :image).merge(user_id: current_user.id)
+  end
+
+  def find
+    @web_app = WebApp.find(params[:id])
+  end
+
+  def move_to_top
+    if current_user.id != @web_app.user.id
+      redirect_to root_path
+    end
   end
 end
