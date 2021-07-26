6ac2b81e128c260981c6a2edefeaa7f834a93022
doshara1280 <danyuan749xa@gmail.com>
2021-07-26 20:41:46 +0900

    ウェブアプリ削除のためのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'web_apps#index'
-  resources :web_apps, only:[:index, :new, :create, :show, :edit, :update]
+  resources :web_apps
 end
