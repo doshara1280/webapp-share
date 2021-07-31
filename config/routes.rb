cb1476173d2ad867a321360cbdc63ff8dfe2794a
doshara1280 <danyuan749xa@gmail.com>
2021-07-31 16:44:50 +0900

    アプリコメントのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'web_apps#index'
-  resources :web_apps
+  resources :web_apps do
+    resources :comments, only: [:create]
+  end
 end
