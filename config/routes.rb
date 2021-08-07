17c672487ba8028157a8cf0d1a8aefe5a89e95fc
doshara1280 <danyuan749xa@gmail.com>
2021-08-07 19:50:51 +0900

    ウェブアプリ説明ページへのリンクを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'web_apps#index'
   resources :web_apps do
+    collection do
+      get 'description'
+    end
     resources :comments, only: [:create]
   end
 end
