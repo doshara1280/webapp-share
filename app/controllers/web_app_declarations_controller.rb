f9e1d082186d8a3e18deb259873a6cdd3d506564
doshara1280 <danyuan749xa@gmail.com>
2021-08-14 17:14:53 +0900

    削除機能の実装、重複部の整理、パスの直接入力によるアクセス防止

--- app/controllers/web_app_declarations_controller.rb
+++ app/controllers/web_app_declarations_controller.rb
@@ -1,4 +1,7 @@
 class WebAppDeclarationsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :find, only: [:show, :destroy]
+
   def index
     @web_app_declarations = WebAppDeclaration.includes(:user).order('created_at DESC').page(params[:page]).per(8)
   end
@@ -17,10 +20,19 @@ class WebAppDeclarationsController < ApplicationController
   end
 
   def show
-    @web_app_declaration = WebAppDeclaration.find(params[:id])
   end
 
+  def destroy
+    @web_app_declaration.destroy
+    redirect_to web_app_declarations_path
+  end
+
+  private
   def web_app_declaration_params
     params.require(:web_app_declaration).permit(:name, :description, :days, :image).merge(user_id: current_user.id)
+  end
+
+  def find
+    @web_app_declaration = WebAppDeclaration.find(params[:id])
   end
 end
