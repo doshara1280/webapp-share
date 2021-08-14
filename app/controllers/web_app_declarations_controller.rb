9948e343de051e544a8b6e4b4a0d8a538ffe86a2
doshara1280 <danyuan749xa@gmail.com>
2021-08-14 12:11:36 +0900

    投稿宣言用のコントローラー作成

---
+++ app/controllers/web_app_declarations_controller.rb
@@ -0,0 +1,22 @@
+class WebAppDeclarationsController < ApplicationController
+  def index
+    @web_app_declarations = WebAppDeclaration.includes(:user).order('created_at DESC').page(params[:page]).per(8)
+  end
+
+  def new
+    @web_app_declaration = WebAppDeclaration.new
+  end
+
+  def create
+    @web_app_declaration = WebAppDeclaration.new(web_app_declaration_params)
+    if @web_app_declaration.save
+      redirect_to web_app_declarations_path
+    else
+      render :new
+    end
+  end
+
+  def web_app_declaration_params
+    params.require(:web_app_declaration).permit(:name, :description, :days, :image).merge(user_id: current_user.id)
+  end
+end
