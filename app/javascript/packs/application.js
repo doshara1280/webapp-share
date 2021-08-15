2d1d4d0326763069a52efc6da33b72580c945456
doshara1280 <danyuan749xa@gmail.com>
2021-08-15 21:54:29 +0900

    投稿宣言に対するコメントを非同期通信

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("@rails/ujs").start()
 require("@rails/activestorage").start()
 require("channels")
 require("../comment")
+require("../declaration_comment")
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
